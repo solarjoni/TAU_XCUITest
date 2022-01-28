3d4e059b8c26362deb251f98daba71db608649a0
solarjoni <solarjoni@gmail.com>
2022-01-28 14:35:42 +0200

    8 - Setting up XCUITest on CI

--- TAUUITests/Base/TAUUITestBase.swift
+++ TAUUITests/Base/TAUUITestBase.swift
@@ -17,7 +17,7 @@ class TAUUITestBase: XCTestCase {
 
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         app.launch()
-        eyes.apiKey = "2iyWHNjbe76FFCRIAsnb2CIA7Va109ExkAK3C102RkEJRng110"
+        eyes.apiKey = ProcessInfo.processInfo.environment["APPLITOOLS_API_KEY"]!
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
